ffa8188cf810b6ace0745c6777f62108c714b2e9
varshith <sarasanivarshith@gmail.com>
2024-08-04 11:10:17 +0530

    added

--- script.js
+++ script.js
@@ -47,7 +47,8 @@ function updateDOM(data) {
 
 async function fetchWeather(location) {
 
-    const url = `http://api.weatherapi.com/v1/current.json?key=4f00639c2db9450dab713942240408&q=${location}&aqi=yes
+    const url = `http://api.weatherapi.com/v1/current.json?key=4f00639c2db9450dab713942240408&q=${location}
+    &aqi=yes
 `
     // fetch -> inbuilt function to get http response from a server
     const response = await fetch(url);
